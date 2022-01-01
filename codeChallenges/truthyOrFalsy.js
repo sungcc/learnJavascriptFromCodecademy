e208552a17c91df6283a39652d3f593c18c6d3f8
sungcc <samooies@yahoo.com.hk>
2022-01-02 02:31:03 +0800

    new challenge

---
+++ codeChallenges/truthyOrFalsy.js
@@ -0,0 +1,20 @@
+// Write your function here:
+
+const truthyOrFalsy = (value) => {
+  if (value) {
+    return true;
+  } else {
+    return false;
+  } 
+}
+
+
+
+
+
+// Uncomment the line below when you're ready to try out your function
+console.log(truthyOrFalsy(1)) // Should print false
+console.log(truthyOrFalsy(0))
+
+// We encourage you to add more function calls of your own to test your code!
+
